25ded85731a192f6ef2736587e8a954af57a14d3
Nick Kelley <nkelley1@berklee.edu>
2020-04-07 19:12:47 -0700

    reorganize project structure

---
+++ server/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from sys import path
+# gives us access to the non-public facing modules
+path.append("../private")
+
+from api import zoom
+
+print("Content-type: text/plain\n")
+print("I haven't made this yet. go away.\n")

2134317ba09ae3eee4e4fe10da5844a2e7557287
Nick <nick@nickkelleystuff.com>
2020-04-10 19:13:30 -0700

    Cleanup build/deploy scripts

---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+root="$(git rev-parse --show-toplevel)"
+
+"$root/bin/build.sh"
+
+source "$root/config.sh"
+
+echo "Deploying to $DEPLOYHOST:$DEPLOYPATH"
+rsync -a "$root/public/" "$DEPLOYHOST":"$DEPLOYPATH"
+echo "Done"

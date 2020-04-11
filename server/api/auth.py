609bce180fbb844f230cb315f0adfec0c0217e18
Nick <nick@nickkelleystuff.com>
2020-04-11 11:48:30 -0700

    add auth skeleton

---
+++ server/api/auth.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import os
+
+def is_authenticated():
+    return False
--- server/api/main.py
+++ server/api/main.py
@@ -4,6 +4,11 @@ from sys import path
 # gives us access to the non-public facing modules
 path.append("../private")
 
-import zoom, request
+import zoom
+import request
+import auth
 
-request.send_response(200, "Not implemented yet")
+if auth.is_authenticated():
+    request.send_response(200, "Not implemented")
+else:
+    request.send_response(401, "Not logged in")

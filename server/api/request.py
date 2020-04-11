199a9be6b2928ff411af9ecda41b10cb10b165d5
Nick <nick@nickkelleystuff.com>
2020-04-11 12:34:22 -0700

    include status in request

--- server/api/request.py
+++ server/api/request.py
@@ -4,5 +4,7 @@ import cgi
 import json
 
 def send_response(status, data):
-    print("Content-type: application/json\n")
+    print("Content-type: application/json")
+    print("Status:{}".format(status))
+    print()
     print(json.dumps(data))

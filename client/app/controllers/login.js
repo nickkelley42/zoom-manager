608a5955f6b21ff591ea8034b64563a6f754dce3
Nick <nick@nickkelleystuff.com>
2020-04-10 22:33:52 -0700

    Basic login route

---
+++ client/app/controllers/login.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+export default class LoginController extends Controller {
+  username = "";
+  password = "";
+
+  actions = {
+    login() {
+    }
+  };
+}

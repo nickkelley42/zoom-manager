95faaf9eca1d24d447e92ea68b73a1a1a8d7c526
Nick <nick@nickkelleystuff.com>
2020-04-16 20:48:49 -0700

    JWT encoding function

--- server/api/zoom.py
+++ server/api/zoom.py
@@ -1,2 +1,21 @@
-def test():
-    print("Hello, world!")
+#!/usr/bin/env python3
+
+import jwt
+import config
+from time import time
+from math import floor
+
+def get_jwt():
+    return jwt.encode(
+        {
+            'iss': config.zoom_api_key,
+            'exp': expiration()
+        },
+        config.zoom_api_secret,
+        algorithm = 'HS256'
+    )
+    
+def expiration():
+    # 5 minutes from now
+    return floor(time() + (5 * 60))
+

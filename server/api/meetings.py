1a7ed37bfbe9532631f15f4035370d474b135399
Nick <nick@nickkelleystuff.com>
2020-04-17 19:16:42 -0700

    format api data according to jsonapi spec

--- server/api/meetings.py
+++ server/api/meetings.py
@@ -3,6 +3,21 @@ import request
 
 def get_all_meetings():
     meetings = zoom.get_meetings()
+    resp_meetings = []
+
+    for meeting in meetings:
+        formatted = {
+            'type': 'meetings',
+            'id': meeting['id'],
+            'attributes': {
+                'topic': meeting['topic'],
+                'start-time': meeting['start_time'],
+                'join-url': meeting['join_url'],
+                'duration': meeting['duration'],
+            }
+        }
+        resp_meetings.append(formatted)
+
     response = request.Response()
-    response.data = { 'meeting': meetings }
+    response.data = { 'data': resp_meetings }
     response.send()

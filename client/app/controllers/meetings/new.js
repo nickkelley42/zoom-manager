b2a9662021a8d5e482eddca2826658d50a777a5b
Nick <nick@nickkelleystuff.com>
2020-04-18 16:13:57 -0700

    linting fixes

--- client/app/controllers/meetings/new.js
+++ client/app/controllers/meetings/new.js
@@ -5,7 +5,10 @@ export default class MeetingsNewController extends Controller {
   @action submit() {
     let meeting = this.get('model');
     this.set('message', '...');
+    this.set('sending', true);
     meeting.save().then(() => {
+      this.set('sending', false);
+      this.set('message', '');
       this.transitionToRoute('meetings.index');
     });
   }
